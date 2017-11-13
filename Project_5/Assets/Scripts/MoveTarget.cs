166c530995d96d4b41c48e39455c9bea23e67070
SethPain <spm2667@louisiana.edu>
2017-11-12 23:40:29 -0600

    Scenes Created
    The scenes have been created (walls, terrains), and the script to get
    the target to move to a clicked position has been written.

---
+++ Project_5/Assets/Scripts/MoveTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour {
+    Vector3 newPosition;
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+                transform.position = new Vector3(newPosition.x, 1, newPosition.z);
+            }
+        }
+    }
+    /*
+    Vector3 current;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(current != transform.position)
+        {
+            transform.Translate(current.x, 0, current.z);
+        }
+	}
+
+    void OnMouseDown()
+    {
+        current = Input.mousePosition;
+    }
+    */
+}
